f007cad539e85a9b54d893146dbe2bb20c61bcd7
Jakub Zając <jakub.zajac2611@gmail.com>
2023-04-20 20:38:37 +0200

    added site

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const nav= document.querySelector('.navbar')
+    const allNavItems = document.querySelectorAll('.nav-link')
+    const navList = document.querySelector('.navbar-collapse')
+
+    function addShadow(){
+        if(window.scrollY>=300){
+            nav.classList.add('shadow-bg')
+        }
+        else{
+            nav.classList.remove('shadow-bg')
+        }
+    }
+
+    allNavItems.forEach(item => this.addEventListener('click', ()=> navList.classList.remove('show')))
+
+    
+    window.addEventListener('scroll', addShadow);
+
+})
+
+
---
+++ src/js/slicksetting.js
@@ -0,0 +1,24 @@
+$('.team-carousel').slick(
+    
+    {
+        arrows:false,
+        autoplay:true,
+        mobileFirst:true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive:[{
+            breakpoint:768,
+            settings: {
+                slidesToShow:2
+            }
+        },
+        {
+            breakpoint:992,
+            settings: {
+                slidesToShow:3
+            }
+        }]
+    }
+
+  );
